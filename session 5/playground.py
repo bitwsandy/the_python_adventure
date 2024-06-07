7ac3f2de915b37267138b6f22f97968482094d85
Sandeep <sdp117@gmail.com>
2024-06-07 09:06:26 +0530

    updated playground.py : bloodline_inheritance

--- session 5/playground.py
+++ session 5/playground.py
@@ -1,21 +1,51 @@
-def print_details(name, age=None, job=None):
-    """Prints details about a person, handling multiple scenarios.
+class Employee :
+    def __init__(self,name, email):
+        self.name = name
+        self.email = email
+    def display_info(self):
+        print(f"Name : {self.name}, email : {self.email}")
 
-    Args:
-        name (str): The person's name.
-        age (int, optional): The person's age. Defaults to None.
-        job (str, optional): The person's job. Defaults to None.
-    """
-    details = f"Name: {name}"
-    if age:
-        details += f", Age: {age}"
-    if job:
-        details += f", Job: {job}"
-    print(details)
+class Developer(Employee):
+    def __init__(self, name, email, programming_language):
+        super().__init__(name, email)
+        self.programming_language = programming_language
+    def display_info(self):
+        super(Developer, self).display_info()
+        print(f"Programming Language : {self.programming_language}")
+
+class Manager(Employee):
+    def __init__(self, name, email, emp_list=None):
+        super().__init__(name, email)
+        if emp_list == None:
+            self.emp_list=[]
+        else :
+            self.emp_list = emp_list
+
+    def add_emp(self,emp):
+        if emp not in self.emp_list :
+            self.emp_list.append(emp.name)
+
+    def rem_emp(self, emp):
+        if emp in self.emp_list :
+            self.emp_list.remove(emp.name)
+
+    def display_info(self):
+        super().display_info()
+        print(f"Number of employees under {self.name} : {self.emp_list}")
+
+emp_1 = Employee("XYZ", "xyz@abc.com")
+# print(type(emp_1))
+
+dev_1 = Developer("John","john@gmail.com","python")
+dev_1.display_info()
+
+mgr_1 = Manager("Bob", "bob@gmail.com")
+mgr_1.display_info()
+mgr_1.add_emp(emp_1)
+mgr_1.add_emp(dev_1)
+print('\n')
+mgr_1.display_info()
 
 
-# Example usage
-print_details("Alice")  # Outputs: Name: Alice
-print_details("Bob", age=25)  # Outputs: Name: Bob, Age: 25
-print_details("Charlie", job="Engineer")  # Outputs: Name: Charlie, Job: Engineer
-print_details("Dana", 34, "Doctor")  # Outputs: Name: Dana, Age: 34, Job: Doctor
+# Polymorphism : NAAM EK KAM ANEK
+
