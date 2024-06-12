ba2a133982a1311e67b7382a48f4cb1d1d624cb1
Sandeep <sdp117@gmail.com>
2024-06-12 08:46:31 +0530

    updated playground.py : bloodline_inheritance

--- session 5/playground.py
+++ session 5/playground.py
@@ -1,51 +1,30 @@
-class Employee :
-    def __init__(self,name, email):
-        self.name = name
-        self.email = email
-    def display_info(self):
-        print(f"Name : {self.name}, email : {self.email}")
+# 5. Create Student class | rno,name,email,marks(int), __cno |
+#    | __get_grade() : Grade | display_info : rno,name,email,cno,grade |
 
-class Developer(Employee):
-    def __init__(self, name, email, programming_language):
-        super().__init__(name, email)
-        self.programming_language = programming_language
-    def display_info(self):
-        super(Developer, self).display_info()
-        print(f"Programming Language : {self.programming_language}")
+class Student:
+    def __init__(self, rno, name, emailid, marks, cno):
+        self.__cn = cno
+        self.rno=rno
+        self.name=name
+        self.emailid=emailid
+        self.marks=marks
 
-class Manager(Employee):
-    def __init__(self, name, email, emp_list=None):
-        super().__init__(name, email)
-        if emp_list == None:
-            self.emp_list=[]
-        else :
-            self.emp_list = emp_list
-
-    def add_emp(self,emp):
-        if emp not in self.emp_list :
-            self.emp_list.append(emp.name)
-
-    def rem_emp(self, emp):
-        if emp in self.emp_list :
-            self.emp_list.remove(emp.name)
+    def __get_grade(self):
+       if self.marks >= 90:
+            return "A"
+       elif self.marks >= 80:
+            return "B"
+       elif self.marks >= 70:  # True
+            return "C"
+       elif self.marks >= 60:
+            return "D"
+       else:
+            return "F"
 
     def display_info(self):
-        super().display_info()
-        print(f"Number of employees under {self.name} : {self.emp_list}")
+        print(f"Rno : {self.rno} | Name : {self.name}")
+        print(f"Email : {self.emailid} | Grade {self.__get_grade()}")
 
-emp_1 = Employee("XYZ", "xyz@abc.com")
-# print(type(emp_1))
-
-dev_1 = Developer("John","john@gmail.com","python")
-dev_1.display_info()
-
-mgr_1 = Manager("Bob", "bob@gmail.com")
-mgr_1.display_info()
-mgr_1.add_emp(emp_1)
-mgr_1.add_emp(dev_1)
-print('\n')
-mgr_1.display_info()
-
-
-# Polymorphism : NAAM EK KAM ANEK
+std_1 = Student(1,"sbc","xyz",91, 89522)
+std_1.display_info()
 
