19bf834dec5748789a3d5639e1183340bec67760
Sandeep Patil <sdp117@gmail.com>
2025-09-11 08:58:54 +0530

    added modules

---
+++ session_4_functions_file_IO_re/simplest_App/list_operations.py
@@ -0,0 +1,9 @@
+
+
+def return_count(l):
+    return len(l)
+
+def deduplicate(l):
+    s = set(l)
+    nl = list(s)
+    return nl
---
+++ session_4_functions_file_IO_re/simplest_App/run_pipeline.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append(r"C:\Users\Sandeep\PycharmProjects\python_hands_on\session_4_functions_file_IO_re\basic_project_modules_1\math_operations")
+from math_operations import add, sub, mult, div
+from list_operations import return_count, deduplicate
+from geometry import area_of_circle
+
+# print(sys.path)
+
+
+# print(add(10, 15))
+# print(sub(10, 15))
+# print(mult(10, 15))
+# print(div(10, 15))
+
+# l1 = [1,1,1,2,3,3,3,5,4,5,4,5,5,2,1,2,2]
+# print(return_count(l1))
+# print(deduplicate(l1))
+
+print(area_of_circle(10))
+
+
