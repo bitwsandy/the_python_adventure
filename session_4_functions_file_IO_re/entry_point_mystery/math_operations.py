33d0df2475fccf87de94453c0d70be17ad468d3f
Sandeep Patil <sdp117@gmail.com>
2025-09-15 09:00:07 +0530

    added entry points

---
+++ session_4_functions_file_IO_re/entry_point_mystery/math_operations.py
@@ -0,0 +1,14 @@
+
+
+from run_pipeline import greet, print_name
+
+def add(a, b):
+    return a + b
+
+print_name()
+print("__name__ for math_ops", __name__)
+
+# greet()
+
+# print("I am from Math Operations")
+# print("Hello, me too")
---
+++ session_4_functions_file_IO_re/entry_point_mystery/run_pipeline.py
@@ -0,0 +1,54 @@
+
+
+# from math_operations import add
+
+def greet():
+    print("Hello from run_pipeline !!!!")
+
+def print_name() :
+    print("I am from run pipeline : ", __name__)
+
+if __name__ == '__main__':
+    print("App deployed")
+    print("Calling functions from modules")
+    # print(add(10, 15))
+    print("App Executed Successfully !!!")
+    print("Cleaning Up !!!")
+    print("Successfully stopped Application")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print("I am from main script")
+#
+#
+# def greet():
+#     print("Good Morning from Main_Script")
+# def main():
+#     print("App deployment started")
+#     print("App deployed")
+#     print("App deployment Ended")
+#
+# print(f"Tha value for __name__ from main scripts is : {__name__}")
+#
+# if __name__ == "__main__" :
+#     main()
+
